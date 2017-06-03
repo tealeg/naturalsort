0d3d257e8a19344cf23faf2150acf520a5827728
Geoffrey J. Teale <tealeg@gmail.com>
2017-06-03 16:32:06 +0200

    Test sorting compounds and subitems

--- naturalsort_test.go
+++ naturalsort_test.go
@@ -49,6 +49,40 @@ func (suite *NaturalSortTestSuite) TestSortMixed() {
 	suite.Equal(item3, items[3])
 }
 
+// When an item is an exact prefix os the item it is compared to then
+// it is sorted ahead of the other item.
+func (suite *NaturalSortTestSuite) TestSortSubItems() {
+	item0 := item{runePart('a'), intPart(1)}
+	item1 := item{runePart('a')}
+	item2 := item{runePart('a'), intPart(1), runePart('a')}
+	items := []item{item0, item1, item2}
+	sort.Sort(ByNaturalOrder(items))
+	suite.Equal(item1, items[0])
+	suite.Equal(item0, items[1])
+	suite.Equal(item2, items[2])
+}
+
+// When items in the item list are compound of intParts and runeParts,
+// natural ordering is achieved.
+func (suite *NaturalSortTestSuite) TestSortCompounds() {
+	item0 := item{runePart('a'), intPart(1)}
+	item1 := item{runePart('a'), intPart(0), runePart('a')}
+	item2 := item{intPart(2), runePart('a')}
+	item3 := item{runePart('b'), intPart(9)}
+	item4 := item{intPart(10), runePart('a')}
+	item5 := item{runePart('b'), intPart(80)}
+	item6 := item{runePart('a'), intPart(0), runePart('a'), runePart('b')}
+	items := []item{item0, item1, item2, item3, item4, item5, item6}
+	sort.Sort(ByNaturalOrder(items))
+	suite.Equal(item2, items[0])
+	suite.Equal(item4, items[1])
+	suite.Equal(item1, items[2])
+	suite.Equal(item6, items[3])
+	suite.Equal(item0, items[4])
+	suite.Equal(item3, items[5])
+	suite.Equal(item5, items[6])
+}
+
 func TestNaturalSortTestSuite(t *testing.T) {
 	suite.Run(t, new(NaturalSortTestSuite))
 }
